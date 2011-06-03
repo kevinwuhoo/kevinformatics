11a8687530ccdca10631408f88033e884a025334
kevin <kevinwuhoo@gmail.com>
2011-06-03 05:50:46 -0700

    fixed title

--- kevinformatics.rb
+++ kevinformatics.rb
@@ -3,22 +3,27 @@ require 'sass'
 
 get '/' do
   @is_index = true
+  @title = ""
   erb :index
 end
 
 get '/about' do
+  @title = "About"
   erb :about
 end
 
 get '/contact' do
+  @title = "Contact"
   erb :contact
 end
 
 get '/projects' do
+  @title = "Projects"
   erb :projects
 end
 
 get %r{(resume)|(cv)} do
+  @title = "Curriculum Vitae"
   erb :cv
 end
 
